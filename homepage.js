c6b764930ba1fd1093bbef8d66ce681471f0c7bc
Sirips1998 <seri1190p@gmail.com>
2025-11-05 07:29:23 +0700

    update hero overlay, navbar z-index, restructure css files

--- homepage.js
+++ homepage.js
@@ -1,10 +1,10 @@
-console.log('Hello, World!');
+console.log("Hello, World!");
 
-document.addEventListener('DOMContentLoaded', () => {
-  const btn = document.querySelector('.menu-btn');
-  const nav = document.querySelector('.nav-links');
-  if(!btn || !nav) return;
-  btn.addEventListener('click', () => {
-    nav.classList.toggle('open');
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector(".menu-btn");
+  const nav = document.querySelector(".nav-links");
+  if (!btn || !nav) return;
+  btn.addEventListener("click", () => {
+    nav.classList.toggle("open");
   });
 });
